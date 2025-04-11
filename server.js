7c9f37b18ca2477f8349bfb2682237d79c948672
SHOBHIT KUMAR <146829121+ShobhitKumar1437@users.noreply.github.com>
2025-04-12 02:15:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/dashboard', (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}`);
 });
